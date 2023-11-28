b1ab6dba0d6bd03e9014cd5f462041528e991854
KevinDaPlayer <325881@gmail.com>
2023-11-29 05:44:16 +0800

    vaccineSort

--- home.js
+++ home.js
@@ -80,7 +80,7 @@ app.post("/login", (req, res) => {
 });
 
 // 主画面路由
-app.get("/dashboard", (req, res) => {
+/*app.get("/dashboard", (req, res) => {
   // 检查用户是否已登录
   if (req.session.isAuthenticated) {
     // 用户已登录，重定向到主画面
@@ -88,6 +88,75 @@ app.get("/dashboard", (req, res) => {
   } else {
     // 用户未登录，重定向到登录页或其他处理
     res.render("home");
+  }
+});*/
+
+// 主畫面路由
+app.get("/dashboard", (req, res) => {
+  // 檢查用戶是否已登錄
+  if (req.session.isAuthenticated) {
+    const identityCardNumber = req.session.identityCardNumber;
+    if (!identityCardNumber) {
+      return res.status(400).send("Identity Card Number is required");
+    }
+
+    connection.query(
+      "SELECT BirthDate FROM patients WHERE IdentityCardNumber = ?",
+      [identityCardNumber],
+      (err, result) => {
+        if (err) {
+          return res.status(500).send("Database Error");
+        }
+
+        if (result.length === 0) {
+          return res.status(404).send("Patient not found");
+        }
+
+        const birthDate = new Date(result[0].BirthDate);
+        const age = calculateage(birthDate);
+
+        connection.query("SELECT * FROM Vaccine", (err, vaccines) => {
+          if (err) {
+            return res.status(500).send("Database Error");
+          }
+
+          const categories = {
+            past: [],
+            withinYear: [],
+            future: [],
+          };
+
+          vaccines.forEach((vaccine) => {
+            const recommendedAgeInMonths = parseVaccinationTime(
+              vaccine.VaccinationTime
+            );
+            const recommendedAgeInYears = recommendedAgeInMonths / 12;
+            console.log(age);
+            /*if (age >= recommendedAgeInYears) {
+              if (age + 1 > recommendedAgeInYears) {
+                categories.past.push(vaccine); // 已經過了施打疫苗期間
+              } else {
+                categories.withinYear.push(vaccine); // 未來一年內要施打
+              }
+            } else {
+              categories.future.push(vaccine); // 未來超過一年的
+            }*/
+
+            if (age > recommendedAgeInYears) {
+              categories.past.push(vaccine); // 已經過了施打疫苗期間
+            } else if (age + 1 < recommendedAgeInYears) {
+              categories.future.push(vaccine); // 未來超過一年的
+            } else {
+              categories.withinYear.push(vaccine); // 未來一年內要施打
+            }
+          });
+
+          res.render("home", { categories });
+        });
+      }
+    );
+  } else {
+    res.redirect("/login");
   }
 });
 
@@ -116,6 +185,68 @@ app.get("/getAge", (req, res) => {
   });
 });
 
+//分類疫苗路由
+app.get("/vaccines", (req, res) => {
+  const identityCardNumber = req.session.identityCardNumber;
+  if (!identityCardNumber) {
+    return res.status(400).send("Identity Card Number is required");
+  }
+
+  db.query(
+    "SELECT BirthDate FROM patient WHERE IdentityCardNumber = ?",
+    [identityCardNumber],
+    (err, result) => {
+      if (err) {
+        return res.status(500).send("Database Error");
+      }
+
+      if (result.length === 0) {
+        return res.status(404).send("Patient not found");
+      }
+
+      const birthDate = new Date(result[0].BirthDate);
+      const age = calculateage(birthDate); // 年齡，以年為單位
+
+      db.query("SELECT * FROM Vaccine", (err, vaccines) => {
+        if (err) {
+          return res.status(500).send("Database Error");
+        }
+
+        const categories = {
+          past: [],
+          withinYear: [],
+          future: [],
+        };
+
+        vaccines.forEach((vaccine) => {
+          const recommendedAgeInMonths = parseVaccinationTime(
+            vaccine.VaccinationTime
+          );
+          const recommendedAgeInYears = recommendedAgeInMonths / 12;
+
+          if (age > recommendedAgeInYears) {
+            categories.past.push(vaccine);
+          } else if (age + 1 > recommendedAgeInYears) {
+            categories.withinYear.push(vaccine);
+          } else {
+            categories.future.push(vaccine);
+          }
+        });
+
+        res.render("home", { categories });
+      });
+    }
+  );
+});
+
+function parseVaccinationTime(vaccinationTime) {
+  // 假設 vaccinationTime 是這樣的格式："出生後5個月"
+  // 解析這個字符串以獲得月份數
+  const match = vaccinationTime.match(/(\d+)/);
+  return match ? parseInt(match[1]) : 0;
+}
+
+//
 function calculateAge(birthDate) {
   var now = new Date();
   var birth = new Date(birthDate);
@@ -132,6 +263,26 @@ function calculateAge(birthDate) {
   }
 
   return age + " 年 " + m + " 個月 " + d + " 天";
+}
+
+function calculateage(birthDate) {
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  let monthDiff = today.getMonth() - birthDate.getMonth();
+
+  // 如果當前月份還沒有到達生日月份，年齡減一
+  if (monthDiff < 0) {
+    age--;
+    monthDiff += 12; // 補上未滿的一年的月份
+  } else if (monthDiff === 0 && today.getDate() < birthDate.getDate()) {
+    age--;
+    monthDiff = 11; // 如果是同月但日期未到，月份應為11（因為已經過了整數年）
+  }
+
+  // 計算月份作為年齡的小數部分
+  const ageWithMonths = age + monthDiff / 12;
+
+  return ageWithMonths;
 }
 
 app.get("/", (req, res) => {
